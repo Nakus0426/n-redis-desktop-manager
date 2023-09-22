0ee37027de03c1095ee9ad00b3e486c9603a40f0
Nakus0426 <820774459@qq.com>
2023-09-22 18:36:34 +0800

    feat: Add automatic refresh function for connection information

--- src/config/iconifyConfig.ts
+++ src/config/iconifyConfig.ts
@@ -1,9 +1,11 @@
-import { addCollection } from '@iconify/vue'
+import { addCollection, addIcon } from '@iconify/vue'
 import fluent from '@iconify/json/json/fluent.json'
 import twemoji from '@iconify/json/json/twemoji.json'
+import lineMd from '@iconify/json/json/line-md.json'
 
 /** setup iconify */
 export function setupIconify() {
 	addCollection(fluent as any)
 	addCollection(twemoji as any)
+	addCollection(lineMd as any)
 }
--- src/views/connections/overview/Index.vue
+++ src/views/connections/overview/Index.vue
@@ -3,6 +3,11 @@
 		<TSkeleton :loading="connectionInfoLoading" :row-col="skeletonRowCol" animation="gradient">
 			<Header />
 		</TSkeleton>
+		<TTooltip :show-arrow="false" :content="autoRefreshButtonStyle.tooltipContent" placement="left">
+			<button class="auto-refresh" v-ripple @click="handleAutoRefreshClick()">
+				<Icon height="24" width="24" :icon="autoRefreshButtonStyle.icon" />
+			</button>
+		</TTooltip>
 	</div>
 </template>
 
@@ -50,6 +55,16 @@ async function initConnectionInfo() {
 }
 onMounted(() => initConnectionInfo())
 provide(connectionInfoInjectKey, connectionInfo)
+
+// auto refresh
+const autoRefreshEnable = ref(false)
+const autoRefreshButtonStyle = computed(() => ({
+	icon: autoRefreshEnable.value ? 'line-md:loading-loop' : 'fluent:arrow-sync-24-regular',
+	tooltipContent: autoRefreshEnable.value ? '关闭自动刷新' : '开启自动刷新',
+}))
+function handleAutoRefreshClick() {
+	autoRefreshEnable.value = !autoRefreshEnable.value
+}
 </script>
 
 <style scoped lang="scss">
@@ -61,5 +76,35 @@ provide(connectionInfoInjectKey, connectionInfo)
 
 :deep(.t-skeleton__col) {
 	background-color: var(--td-bg-color-component);
+}
+
+.auto-refresh {
+	position: absolute;
+	right: var(--td-comp-paddingLR-m);
+	bottom: var(--td-comp-paddingLR-m);
+	width: 42px;
+	height: 42px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	border: 1px solid var(--td-component-border);
+	border-radius: var(--td-radius-medium);
+	background-color: var(--td-bg-color-container);
+	color: var(--td-text-color-primary);
+	box-shadow: var(--td-shadow-3);
+	transition: all var(--td-transition);
+	cursor: pointer;
+	z-index: 500;
+	--ripple-color: var(--td-bg-color-container-active);
+
+	&:hover {
+		background-color: var(--td-bg-color-container-hover);
+	}
+
+	&:focus-visible {
+		outline: none;
+		border-color: var(--td-brand-color);
+		color: var(--td-brand-color);
+	}
 }
 </style>
