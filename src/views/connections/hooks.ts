a07badedc41b390f97b68d3eb44851467178068d
Nakus0426 <820774459@qq.com>
2023-10-11 15:43:15 +0800

    perf: Optimize the performance of converting keys from an array to a tree

--- src/views/connections/hooks.ts
+++ src/views/connections/hooks.ts
@@ -1,33 +1,22 @@
-import { flatMapDeep, uniqWith } from 'lodash-es'
-
 /**
  * transform keys to tree
  */
 export function useKeyTree(keys: string[], separator: string) {
-	const transformedKeys = flatMapDeep(
-		keys.map(key => {
-			const splitKeys = key.split(separator)
-			return splitKeys.map((splitKey, index) => {
-				const label = splitKey === '' ? '[Empty]' : splitKey
-				const isLeaf = index === splitKeys.length - 1
-				const value = isLeaf ? key : splitKeys.slice(0, index + 1).join(separator)
-				const parent = index === 0 ? undefined : splitKeys.slice(0, index).join(separator)
-				return { label, parent, isLeaf, value }
-			})
-		})
-	)
-	const uniqKeys = uniqWith(
-		transformedKeys,
-		(a, b) => a.label === b.label && a.parent === undefined && b.parent === undefined && a.value === b.value
-	)
-	const keysMap = new Map(uniqKeys.map(item => [item.value, { ...item, children: [] }]))
 	const tree = []
-	for (const item of keysMap.values()) {
-		if (item.parent) {
-			keysMap.get(item.parent).children.push(item)
-		} else {
-			tree.push(item)
-		}
-	}
+	keys.forEach(key => {
+		const splitKeys = key.split(separator)
+		const splitKeysLength = splitKeys.length
+		let node = tree
+		splitKeys.forEach((key2, index2) => {
+			key2 = key2 === '' ? '[Empty]' : key2
+			if (!node.find(item => item.label === key2)) node.push({ label: key2, children: [], isLeaf: false, value: null })
+			const foundNode = node.find(item => item.label === key2)
+			if (index2 === splitKeysLength - 1) {
+				foundNode.isLeaf = true
+				foundNode.value = key
+			}
+			node = foundNode.children
+		})
+	})
 	return tree
 }
